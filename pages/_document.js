3e962edaa3a598963c4e8bed363538492df05035
Lester Cardoz <lestercardoz11@gmail.com>
2021-05-14 17:45:25 +0530

    Fixed ModuleNotFoundError

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import theme from '@hooks/theme';
+import theme from '@/hooks/theme';
 
 export default class Document extends NextDocument {
   render() {
