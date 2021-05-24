ed8eb12b80f6292623aa45f261b27d236dd2d71f
Lester Cardoz <lestercardoz11@gmail.com>
2021-05-24 13:25:53 +0530

    Head, 404 and minor fixes

--- src/components/social.js
+++ src/components/social.js
@@ -1,79 +1,51 @@
-import styled from 'styled-components';
 import { email, socialMedia } from '@/config';
-import { Box, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Link,
+  useColorModeValue,
+  SlideFade,
+  IconButton,
+} from '@chakra-ui/react';
 import { Icon } from '@/components/icons';
-
-const StyledSocialList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  &:after {
-    content: '';
-    display: block;
-    width: 1px;
-    height: 90px;
-    margin: 0 auto;
-    background-color: ${(props) => props.color ?? props.color};
-  }
-  li {
-    &:last-of-type {
-      margin-bottom: 20px;
-    }
-    a {
-      padding: 10px;
-      &:hover,
-      &:focus {
-        transform: translateY(-3px);
-      }
-      svg {
-        width: 20px;
-        height: 20px;
-      }
-    }
-  }
-`;
-
-const StyledLinkWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  &:after {
-    content: '';
-    display: block;
-    width: 1px;
-    height: 90px;
-    margin: 0 auto;
-    background-color: ${(props) => props.color ?? props.color};
-  }
-  a {
-    margin: 20px auto;
-    padding: 10px;
-    letter-spacing: 2px;
-    writing-mode: vertical-rl;
-    border: 2px solid transparent;
-    border-radius: 5px;
-    transition: border ease 0.2s;
-    &:hover,
-    &:focus {
-      border-color: ${(props) => props.color ?? props.color};
-    }
-  }
-`;
 
 const Email = () => (
   <Box
     position={'absolute'}
     bottom={0}
-    right={16}
-    display={{ base: 'none', md: 'flex' }}
-  >
-    <StyledLinkWrapper color={useColorModeValue('#1A202C', 'white')}>
-      <a href={`mailto:${email}`}>{email}</a>
-    </StyledLinkWrapper>
+    right={12}
+    display={{ base: 'none', md: 'flex' }}>
+    <SlideFade in={true} offsetY={100}>
+      <Flex
+        direction='column'
+        align='center'
+        justify='center'
+        position='relative'
+        _after={{
+          content: `''`,
+          display: 'block',
+          width: '1px',
+          height: '90px',
+          margin: '0 auto',
+          backgroundColor: useColorModeValue('gray.600', 'white'),
+        }}>
+        <Link
+          href={`mailto:${email}`}
+          m='20px auto'
+          p='10px'
+          letterSpacing='2px'
+          border='2px solid transparent'
+          borderRadius='5px'
+          sx={{ writingMode: 'vertical-rl' }}
+          _hover={{
+            textDecoration: 'none',
+            transform: 'translateY(-2.5px)',
+            transition: 'transform ease 0.2s',
+          }}>
+          {email}
+        </Link>
+      </Flex>
+    </SlideFade>
   </Box>
 );
 
@@ -81,24 +53,43 @@ const SocialLinks = () => (
   <Box
     position={'absolute'}
     bottom={0}
-    left={16}
-    display={{ base: 'none', md: 'flex' }}
-  >
-    <StyledSocialList color={useColorModeValue('#1A202C', 'white')}>
-      {socialMedia &&
-        socialMedia.map(({ url, name }, i) => (
-          <li key={i}>
-            <a
-              href={url}
-              aria-label={name}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <Icon name={name} />
-            </a>
-          </li>
-        ))}
-    </StyledSocialList>
+    left={12}
+    display={{ base: 'none', md: 'flex' }}>
+    <SlideFade in={true} offsetY={100}>
+      <Flex
+        direction='column'
+        align='center'
+        justify='center'
+        position='relative'
+        _after={{
+          content: `''`,
+          display: 'block',
+          width: '1px',
+          height: '90px',
+          margin: '0 auto',
+          backgroundColor: useColorModeValue('gray.600', 'white'),
+        }}>
+        <Flex
+          mb='20px'
+          px='10px'
+          direction='column'
+          align='center'
+          justify='center'>
+          {socialMedia &&
+            socialMedia.map(({ url, name }, i) => (
+              <Box h={5} w={5} my={5} key={i}>
+                <Link
+                  href={url}
+                  aria-label={name}
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <Icon name={name} />
+                </Link>
+              </Box>
+            ))}
+        </Flex>
+      </Flex>
+    </SlideFade>
   </Box>
 );
 
